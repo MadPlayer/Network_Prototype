36575404aee4ef2ed20457830b8379f8eacb6728
MadPlayer <kch9001@gmail.com>
2022-05-24 15:07:32 +0900

    fix concurrency bug in MQTT-AMQP

--- MQTT-AMQP/client.py
+++ MQTT-AMQP/client.py
@@ -17,42 +17,44 @@ class MQTTClient:
         self.client = Client(hostname=hostname, username=username,
                              password=password)
         await self.client.connect()
-
         return self
 
-    async def test(self, future, reply_topic):
+    async def request(self):
+        future = self.loop.create_future()
+        self.loop.create_task(self.request_(future))
+        return future
+
+    async def request_(self, future):
+        call_id = str(uuid.uuid4())
+        reply_topic = f"test/{call_id}"
+        await self.client.subscribe(reply_topic)
+        self.loop.create_task(
+            self.client.publish(
+                "rpc_queue",
+                payload=pickle.dumps({
+                    "data": "hello world",
+                    "reply_to": reply_topic,
+                }),
+                qos=1,
+            )
+        )
+        print(f"reply_to {reply_topic}")
+
         async with self.client.filtered_messages(reply_topic) as msgs:
             async for msg in msgs:
                 print(f"outcome: {msg.payload.decode()}")
                 await self.client.unsubscribe(reply_topic)
                 future.set_result(msg)
-                return
-
-    async def request(self):
-        call_id = str(uuid.uuid4())
-        reply_topic = f"test/{call_id}"
-        await self.client.subscribe(reply_topic)
-        await self.client.publish(
-            "rpc_queue",
-            payload=pickle.dumps({
-                "data": "hello world",
-                "reply_to": reply_topic,
-            }),
-            qos=1,
-        )
-        print(f"reply_to {reply_topic}")
-
-        future = self.loop.create_future()
-        self.loop.create_task(self.test(future, reply_topic))
-        return future
 
 
 async def main():
     client = await MQTTClient().connect(URL, "client1", "test")
-    future = await client.request()
-    print("middle")
-    msg = await future
-    print(f"outcome2: {msg.payload.decode()}")
+
+    for i in range(10000):
+        future = await client.request()
+        print("middle")
+        msg = await future
+        print(f"outcome2: {msg.payload.decode()}")
 
 
 if __name__ == '__main__':
