a7d8bcbf1c06d86685ad12f2f39a0d3c846c8628
MadPlayer <kch9001@gmail.com>
2022-05-31 23:23:29 +0900

    declare common package for every protocol tests

---
+++ testing/the_sieve_of_Eratosthenes/common_package/__init__.py
@@ -0,0 +1,14 @@
+from .test_pb2_grpc import (
+    PrimeCalculate,
+    add_PrimeCalculateServicer_to_server,
+    PrimeCalculateStub,
+)
+from .test_pb2 import NumberRange, Response
+
+__all__=(
+    PrimeCalculate,
+    add_PrimeCalculateServicer_to_server,
+    PrimeCalculateStub,
+    NumberRange,
+    Response,
+)
