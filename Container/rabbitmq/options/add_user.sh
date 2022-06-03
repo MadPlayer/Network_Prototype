e71b7765ac1b6cbf5b8f3329fc061a96fe957d0b
MadPlayer <kch9001@gmail.com>
2022-06-04 01:22:18 +0900

    rabbitmq automize add_ser

---
+++ Container/rabbitmq/options/add_user.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+passwd=test
+
+if [[ -z $1 ]]; then
+	echo admin!
+	rabbitmqctl add_user admin admin
+	rabbitmqctl set_user_tags admin administrator
+	rabbitmqctl set_permissions -p / admin \".*\" \".*\" \".*\"
+	rabbitmqctl set_topic_permissions -p / admin amq.topic \".*\" \".*\"
+	exit 0
+fi
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		* )
+		rabbitmqctl add_user $1 $passwd
+		rabbitmqctl set_permissions -p / $1 \".*\" \".*\" \".*\"
+		rabbitmqctl set_topic_permissions -p / $1 amq.topic \".*\" \".*\"
+		;;
+	esac
+	shift
+done
+
+
