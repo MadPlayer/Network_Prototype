333b299830068cbad0b3c5b1d48203f0dd2e13c0
MadPlayer <kch9001@gmail.com>
2022-06-04 16:30:37 +0900

    fix rabbitmq script bug

--- Container/rabbitmq/options/add_user.sh
+++ Container/rabbitmq/options/add_user.sh
@@ -6,8 +6,8 @@ if [[ -z $1 ]]; then
 	echo admin!
 	rabbitmqctl add_user admin admin
 	rabbitmqctl set_user_tags admin administrator
-	rabbitmqctl set_permissions -p / admin \".*\" \".*\" \".*\"
-	rabbitmqctl set_topic_permissions -p / admin amq.topic \".*\" \".*\"
+	rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+	rabbitmqctl set_topic_permissions -p / admin amq.topic ".*" ".*"
 	exit 0
 fi
 
@@ -15,8 +15,8 @@ while [ $# -gt 0 ]; do
 	case "$1" in
 		* )
 		rabbitmqctl add_user $1 $passwd
-		rabbitmqctl set_permissions -p / $1 \".*\" \".*\" \".*\"
-		rabbitmqctl set_topic_permissions -p / $1 amq.topic \".*\" \".*\"
+		rabbitmqctl set_permissions -p / $1 ".*" ".*" ".*"
+		rabbitmqctl set_topic_permissions -p / $1 amq.topic ".*" ".*"
 		;;
 	esac
 	shift
